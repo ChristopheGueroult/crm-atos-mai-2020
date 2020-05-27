f724ba266369ec9840237d7787a6d6d9ace3a80a
christophe gueroult <christophegueroult@gmail.com>
2020-05-27 15:46:07 +0100

    @Output()

--- src/app/orders/pages/page-list-orders/page-list-orders.component.ts
+++ src/app/orders/pages/page-list-orders/page-list-orders.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Order } from 'src/app/shared/models/order';
 import { OrdersService } from '../../services/orders.service';
 import { StateOrder } from 'src/app/shared/enums/state-order.enum';
+import { Btn } from 'src/app/shared/interfaces/btn';
 
 @Component({
   selector: 'app-page-list-orders',
@@ -11,10 +12,25 @@ import { StateOrder } from 'src/app/shared/enums/state-order.enum';
 export class PageListOrdersComponent implements OnInit {
   public collection: Order[];
   public headers: string[];
+  public btnRoute: Btn;
+  public btnHref: Btn;
+  public btnAction: Btn;
   public states = Object.values(StateOrder);
   constructor(private os: OrdersService) { }
 
   ngOnInit(): void {
+    this.btnRoute = {
+      label: 'Add an order',
+      route: 'add'
+    };
+    this.btnHref = {
+      label: 'Go to Google',
+      href: 'http://www.google.fr'
+    };
+    this.btnAction = {
+      label: 'Open dialogue',
+      action: true
+    };
     this.os.collection.subscribe((datas) => {
       this.collection = datas;
     });
@@ -34,7 +50,10 @@ export class PageListOrdersComponent implements OnInit {
       // traiter la res de l'api, codes erreur etc...
       item.state = res.state;
     });
+  }
 
+  public openPopUp() {
+    console.log('open popup');
   }
 
 }
