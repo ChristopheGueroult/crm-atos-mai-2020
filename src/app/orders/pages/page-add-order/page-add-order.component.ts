cf73c3787f4b7d588c4b54dcb29715aa9dc98b10
christophe gueroult <christophegueroult@gmail.com>
2020-05-28 14:53:02 +0100

    ReactiveFormsModule and navigate() ts

---
+++ src/app/orders/pages/page-add-order/page-add-order.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Order } from 'src/app/shared/models/order';
+import { OrdersService } from '../../services/orders.service';
+
+@Component({
+  selector: 'app-page-add-order',
+  templateUrl: './page-add-order.component.html',
+  styleUrls: ['./page-add-order.component.scss']
+})
+export class PageAddOrderComponent implements OnInit {
+  public title: string;
+  public subtitle: string;
+  constructor(
+    private os: OrdersService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.title = 'Orders';
+    this.subtitle = 'Add an order';
+  }
+
+  public add(item: Order) {
+    this.os.add(item).subscribe((res) => {
+      // console.log(res);
+      // redirection vers la route orders
+      // this.router.navigate(['orders']);
+      // redirection relative par rapport à la route sur laquelle je suis
+      this.router.navigate(['../'], {relativeTo: this.route});
+    });
+  }
+
+}
