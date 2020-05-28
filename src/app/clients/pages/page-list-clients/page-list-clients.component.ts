e0ebcd967c033b40a5df1d193c0dd7ae495d03fd
christophe gueroult <christophegueroult@gmail.com>
2020-05-28 13:02:12 +0100

    tp page list clients

--- src/app/clients/pages/page-list-clients/page-list-clients.component.ts
+++ src/app/clients/pages/page-list-clients/page-list-clients.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Client } from 'src/app/shared/models/client';
+import { Btn } from 'src/app/shared/interfaces/btn';
+import { StateClient } from 'src/app/shared/enums/state-client.enum';
+import { ClientsService } from '../../services/clients.service';
 
 @Component({
   selector: 'app-page-list-clients',
@@ -6,10 +11,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-list-clients.component.scss']
 })
 export class PageListClientsComponent implements OnInit {
-
-  constructor() { }
+  public collection$: Observable<Client[]>;
+  public headers: string[];
+  public btnRoute: Btn;
+  public title: string;
+  public subtitle: string;
+  public states = Object.values(StateClient);
+  constructor(private cs: ClientsService) { }
 
   ngOnInit(): void {
+    this.title = 'Clients';
+    this.subtitle = 'All clients';
+    this.btnRoute = {
+      label: 'Add a client',
+      route: 'add'
+    };
+    this.collection$ = this.cs.collection;
+    this.headers = [
+      'Name',
+      'CA HT',
+      'CA TTC',
+      'State'
+    ];
   }
 
+  public changeState(item: Client, event) {
+    this.cs.changeState(item, event.target.value).subscribe((res) => {
+      // traiter la res de l'api, codes erreur etc...
+      item.state = res.state;
+    });
+  }
 }
--- todo.md
+++ todo.md
@@ -1,4 +1,20 @@
-- templates de pages
-- :host :host-context()
-- app-text component
+
 - TP page clients
+
+- Créer un enum state-client ACTIVE et INACTIVE
+- Créer le model client et l'interface client-i (voir db.json)
+- Créer le service clients 
+- Page clients
+  - template-b
+  - pipe total
+  - directive state-directive
+  - scss table-light et table-dark (.state-active $color-success) (.state-inactive $color-error)
+
+- formulaires
+- validation de formulaires
+- passer des datas dans le route
+- récupérer les datas avec ActivateRoute
+- TP : sous navigation dans une page avec routes enfants
+- TP : exemple BehaviorSubject et Subject
+- TP : partie delete (Observables chauds)
+- TP : partie edit (passage de datas dans l'url) et ActivatedRoute pour récupérer les datas
